a96380de7b7bfe42fac64733ee528686366af03c
Teo <talairla@educa.aragon.es>
2016-12-13 18:17:46 +0100

    Hilos

--- hilos/holamundo_hilos.c
+++ hilos/holamundo_hilos.c
@@ -6,6 +6,11 @@
 
 void* hola(void*);
 void* mundo(void*);
+void* imprimir(void*);
+
+typedef struct{
+	char texto[100];
+}cadena_100;
 
 int main(int argc, char const *argv[])
 {
@@ -13,13 +18,34 @@ int main(int argc, char const *argv[])
 	pthread_t hilo_hola;
 	pthread_t hilo_mundo;
 
-	pthread_create(&hilo_hola, NULL, hola, &parametro1);
-	pthread_create(&hilo_mundo, NULL, mundo, NULL);
+	cadena_100 msg1;
+	cadena_100 msg2;
+	strcpy(msg1.texto,"Hola");
+	strcpy(msg2.texto,"Mundo");
+	cadena_100* parametro = &msg1;
+
+	pthread_create(hilo_hola, NULL ,  , parametro);
 
 	pthread_join(hilo_hola, NULL);
 	pthread_join(hilo_mundo, NULL);
 	return 0;
 }
+////SIN ACABAR///
+void* imprimir(void* parametros){
+	char* msg = "Hola";
+	int i;
+	fprintf(stderr, "En la posicion %p se encuentra el entero: %d\n",
+					 parametros, *( (int*) parametros ) );
+
+	for (i=0; i<strlen(msg); i++){
+		printf("%c\n",msg[i]);
+		fflush(stdout);
+		//usleep(1000000);
+	}
+
+	return NULL;
+}
+
 
 void* hola(void* parametros){
 	char* msg = "Hola";
